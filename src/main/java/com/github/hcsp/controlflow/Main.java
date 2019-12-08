032b4ca7f4aaae9a66738b2fb2c7e8b2cd42ca1c
TangsongyuanmingQingHad <had13625428701@aliyun.com>
2019-12-08 23:40:04 +0800

    递归

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -17,8 +17,6 @@ public class Main {
     public static int factorial(int n) {
         if(n == 0) {
             return 1;
-        }else if(n == 1) {
-            return 1;
         }else {
             return n*factorial(n-1);
         }
