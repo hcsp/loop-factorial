7ab6f3885030c759e23c295f488a3adf23a3a32c
7952126 <15521608275@163.com>
2020-03-27 11:43:00 +0800

    a

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -16,11 +16,11 @@ public class Main {
      */
     public static int factorial(int n) {
         if (n > 1) {
-            return n*factorial(n-1);
+            return n * factorial(n - 1);
         } else if (n == 1) {
-            return  1;
+            return 1;
         } else {
-            return  0;
+            return 1;
         }
     }
 }
