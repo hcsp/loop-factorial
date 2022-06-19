500823a306ee2c67cac36ffd309e4ecdba645dd7
zj <woaiwrf@qq.com>
2022-06-19 17:31:34 +0800

    阶乘

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,7 +15,7 @@ public class Main {
      * @return 该数字的阶乘
      */
     public static int factorial(int n) {
-        if(n==0) return 1;
-        return n*factorial(n-1);
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
     }
 }
